fcc76e206cfb75e7688cb02a3b92d21e02709d7c
RustH3d <humbertoa00327@gmail.com>
2025-06-21 16:29:38 -0400

    nasheee

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express= require('express')
+const app= express()
+const cors= require('cors')
+require('dotenv').config();
+const usersRoutes= require('./routes/users')
+const recipesRoutes= require('./routes/recipes')
+const groupsRoutes= require('./routes/groups')
+const PORT= process.env.PORT || 3000
+const {Pool}= require('pg')
+const pg = require('pg');
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/users',usersRoutes)
+app.use('/recipes',recipesRoutes)
+app.use('/groups',groupsRoutes)
+
+app.get('/',(req,res)=>{
+    res.send('Backend funcionando')
+})
+
+
+
+/* const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recetas",
+  password: "bionicle2006",
+  port: 5432,
+}); */
+
+
+
+ app.listen(PORT,()=>{
+    console.log(`Corriendo en el puerto http://localhost:${PORT}`)
+}) 
